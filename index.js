0bdbb0e9e5454e8504f6e33cd0e7eb9c37e68d26
Joyce-Fernandes <joycecode@hotmail.com>
2022-05-23 16:59:52 +0200

    New commit 23/05/2022

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { IfcViewerAPI } from 'web-ifc-viewer';
-import { Color, Mesh, MeshLambertMaterial } from 'three';
+import {IFCSPACE, IFCOPENINGELEMENT} from 'web-ifc';
+
 
 // Setup IFC
 
@@ -13,8 +14,39 @@ viewer.addAxes();
 viewer.addGrid(100, 100);
 viewer.clipper.active = true;
 viewer.IFC.applyWebIfcConfig({ COORDINATE_TO_ORIGIN: true, USE_FAST_BOOLS: true });
+
       
 
+viewer.context.renderer.usePostproduction = true;
+
+let tree;
+const models = [];
+
+async function loadIFCFromURL(URL) {
+
+
+    await viewer.IFC.loader.ifcManager.setOnProgress((event) => {
+        console.log(event.loaded, event.total);
+    });
+
+    await viewer.IFC.loader.ifcManager.applyWebIfcConfig({
+        USE_FAST_BOOLS: false,
+        COORDINATE_TO_ORIGIN: true
+    });
+
+    await viewer.IFC.loader.ifcManager.parser.setupOptionalCategories({
+        [IFCSPACE]: false,
+        [IFCOPENINGELEMENT]: false,
+      
+    })
+
+    const model = await viewer.IFC.loadIfcUrl(URL);
+    if (model === null) return;
+    models.push(model);
+    tree = await viewer.IFC.getSpatialStructure(model.modelID);
+    console.log(tree);
+    createTreeMenu(model.modelID);
+}
 // Setup GUI
 
 const GUI = {
@@ -26,24 +58,12 @@ const GUI = {
 
 GUI.loader.onclick = () => GUI.input.click();
 
-let ifcProject;
+GUI.input.onchange = async (changed) => {
+    const file = changed.target.files[0];
+    const ifcURL = URL.createObjectURL(file);
 
-GUI.input.onchange = (event) => {
-    const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
-    viewer.IFC.loadIfcUrl(url);
-   
-} 
-
-// load model
-
-async function loadModel() {
-    const model = await viewer.IFC.loadIfcUrl('19-026-L4_UTE_PC01_GEN_MOD_GEN-VIA-VAL_V01.ifc');
-    createTreeMenu(model.modelID);   
-    
+    loadIFCFromURL(ifcURL)
 }
-
-loadModel();
 
 // Highlight items when hovering over them
 
@@ -51,19 +71,41 @@ container.onmousemove = () => viewer.IFC.prePickIfcItem();
 
 // Select items and log properties
 
-container.ondblclick = async () => {
+window.ondblclick = async () => {
     const found = await viewer.IFC.pickIfcItem(true);
-    if(found === null || found === undefined) return;
-    const props = await viewer.IFC.getProperties(found.modelID, found.id, true);
-    updatePropertyMenu(props);
+    if (found === null) return;
+    const properties = await viewer.IFC.getProperties(found.modelID, found.id, true);
+    createPropertiesMenu(properties);
 };
+window.onkeydown = (event) => {
+    if (event.code === 'Escape') {
+        createPropertiesMenu({});
+        viewer.IFC.unpickIfcItems();
+    } else if (event.code === 'KeyC') {
+        viewer.clipper.createPlane();
+    } else if(event.code === 'Delete') {
+        viewer.clipper.deletePlane();
+        viewer.dimensions.delete()
+    } else if(event.code === 'KeyD') {
+        viewer.dimensions.active = true;
+        viewer.dimensions.previewActive = true;
+        viewer.IFC.unpickIfcItems();
+        viewer.IFC.unPrepickIfcItems();
+        container.onmousemove = () => {};
+    } else if(event.code === 'KeyF') {
+        viewer.dimensions.active = false;
+        viewer.dimensions.previewActive = false;
+        viewer.dimensions.create();
+        container.onmousemove = () => viewer.IFC.prePickIfcItem();
+    }
+}
 
 var contTotal; //Variable para guardar la cantidad total de línea a exportar.
 
 // Properties menu
 
-function updatePropertyMenu(props) {
-
+function createPropertiesMenu(props) {
+    console.log(props);
     let contId = 0; //Variable para guardar la cantidad total de línea a exportar.
 
     removeAllChildren(GUI.props);
@@ -190,7 +232,7 @@ function removeAllChildren (element) {
     }
 }
 
-// tree menu
+// Tree view
 var toggler = document.getElementsByClassName("caret");
 
 for (let i = 0; i < toggler.length; i++) {
@@ -200,7 +242,7 @@ for (let i = 0; i < toggler.length; i++) {
         current.classList.toggle("caret-down");
     }
 }
-
+// Spatial tree menu
 async function createTreeMenu(modelID) {
     ifcProject = await viewer.IFC.getSpatialStructure(modelID);
     removeAllChildren(GUI.tree);
@@ -246,11 +288,11 @@ function createNestedNodeTitle(parent, content) {
     title.textContent = content;
     parent.appendChild(title);
 }
-
 function createSimpleChild(parent, node) {
+    const content = nodeToString(node);
     const childNode = document.createElement('li');
     childNode.classList.add('leaf-node');
-    childNode.textContent = nodeToString(node);
+    childNode.textContent = content;
     parent.appendChild(childNode);
 
     childNode.onmouseenter = () => {
@@ -260,10 +302,13 @@ function createSimpleChild(parent, node) {
     childNode.onclick = async () => {
         viewer.IFC.pickIfcItemsByID(0, [node.expressID], true);
         const props = await viewer.IFC.getProperties(0, node.expressID, true);
-        updatePropertyMenu(props);
+        createPropertiesMenu(props);
     }
 }
 
 function nodeToString(node) {
     return `${node.type} - ${node.expressID}`;
 }
+
+
+
